db49d90995a4db8ba76efaceb359cc4ed46f44c7
Chris <Britten63@hotmail.com>
2026-08-11 11:53:27 -0300

    finished off app.jsx import add andaircraft hook components to render on page from api

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import AppNav from "./components/layout/AppNav.jsx";
 import Airports from "./pages/Airports.jsx";
 //cities import
 import Cities from "./pages/Cities.jsx";
+//aircarft import for hooks and components 
+import Aircraft from "./pages/Aircraft.jsx";
+
 
 export default function App() {
   return (
@@ -15,6 +18,7 @@ export default function App() {
             <Route path="/" element={<h1>Home</h1>} />
             <Route path="/airports" element={<Airports />} />
             <Route path="/cities" element={<Cities />} />
+            <Route path="/aircraft" element={<Aircraft />} />
           </Routes>
         </main>
       </div>
